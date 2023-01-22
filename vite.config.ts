6ae76402ece31ce92fdae8fe83e2294fae3041d9
Ariel Saldana <ariel@unreal.io>
2023-01-22 11:49:17 -0500

    optimize imports and exports

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,21 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: true,
+      rollupOptions: {
+        external: [
+            /^node:.*/,
+            "@unreal/pan",
+            "react",
+            "react-dom"
+        ]
+      },
+    minify: false,
     lib: {
       entry: path.resolve(__dirname, 'src/Components.ts'),
       // @ts-ignore
       formats: ['es', 'cjs', 'umd', 'amd'],
       name: 'unreal-components',
       fileName: (format) => `unreal-components.${format}.js`
-    },
-    rollupOptions: {
-      output: {
-        globals: {
-        }
-      }
     },
     target: 'es2015',
     sourceMap: false
